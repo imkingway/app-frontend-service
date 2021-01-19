15047b5056b44e7a4a711c5f4baa4d4ee10d8e05
kingway <tp046721@mail.apu.edu.my>
2021-01-19 19:42:44 +0800

    remove components

--- components/header.js
+++ components/header.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+// import React from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
 
-export default Header = props => {
-    return (
-        <View style = { styles.header }>
-            <Text style = { styles.headerTitle }>
-                { props.title }
-            </Text>
-        </View>
-    )
-}
+// export default Header = props => {
+//     return (
+//         <View style = { styles.header }>
+//             <Text style = { styles.headerTitle }>
+//                 { props.title }
+//             </Text>
+//         </View>
+//     )
+// }
 
-const styles = StyleSheet.create ({
-    header: {
-      width: '100%',
-      height: 20,
-      paddingTop: 25,
-      backgroundColor: '#92ACA7',
-      alignItems: 'center',
-      justifyContent:'center',
-    },
-    headerTitle: {
-        color: 'black',
-        height: 90,
-    },
-  });
+// const styles = StyleSheet.create ({
+//     header: {
+//       width: '100%',
+//       height: 20,
+//       paddingTop: 25,
+//       backgroundColor: '#92ACA7',
+//       alignItems: 'center',
+//       justifyContent:'center',
+//     },
+//     headerTitle: {
+//         color: 'black',
+//         height: 90,
+//     },
+//   });
