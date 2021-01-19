0e06a62753fc3b2d5093533afe73e85e42e4155a
kingway <tp046721@mail.apu.edu.my>
2021-01-19 19:41:47 +0800

    init

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import React from 'react';
+import Providers from './navigation';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
-import BottomNavigation from './components/bottom-navigation';
-import LoginScreen from './screens/loginScreen';
-
-// const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-
-//implement to create side bar 
  const App = () => {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name = "Profile" component = {BottomNavigation} />
-        <Drawer.Screen name = "Sign Out" component = {LoginScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Providers />
   )
 }
 
 export default App;
-
-
 
